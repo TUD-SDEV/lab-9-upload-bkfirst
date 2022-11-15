c8cd36ea415945db0dc7faf364b38ab7545d8836
Brandon Kelly <x00189621@mytudublin.ie>
2022-11-15 16:12:05 +0000

    lab 9 part 2 commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,4 @@ urlpatterns=[
     path('signup/', SignUpView.as_view(), name='signup'),
     path('profile/<int:pk>/', ProfilePageView.as_view(), name='show_profile'),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='edit_profile'),
-
 ]
